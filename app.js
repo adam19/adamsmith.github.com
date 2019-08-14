57c7bfee97aa75547b0960ae7794da44284d742a
Adam Smith <adam.d.smith@disney.com>
2019-08-13 17:18:47 -0700

    Working on the Project Modal window

--- app.js
+++ app.js
@@ -1,125 +1,10 @@
 (function() {
 	var app = angular.module("Portfolio", ['ui.router', 'ngAnimate', 'ui.bootstrap']);
 
-	app.config(function ($stateProvider, $urlRouterProvider) {
-			$urlRouterProvider.otherwise('modal');
-	
-			$stateProvider.state('base', {
-				url: ''
-			});
-
-			$stateProvider.state('modal', {
-				url: "",
-				views:{
-					"modal": {
-						templateUrl: "partials/modal.html"
-					}
-				},
-				onEnter: function($state){
-					// Hitting the ESC key closes the modal
-					$(document).on('keyup', function(e){
-						if(e.keyCode == 27){
-							$(document).off('keyup')
-							$state.go('base')
-						}
-					});
-				
-					// Clicking outside of the modal closes it
-					$(document).on('click', '.Modal-backdrop, .Modal-holder', function() {
-						$state.go('base');
-					});
-				
-					// Clicking on the modal or it's contents doesn't cause it to close
-					$(document).on('click', '.Modal-box, .Modal-box *', function(e) {
-						e.stopPropagation();
-					});
-				},
-				abstract: true
-			});
-
-			$stateProvider.state('modal.disney', {
-				views:{
-					"modal": {
-						templateUrl: 'partials/modalDisney.html'
-					}
-				}
-			});
-			$stateProvider.state('modal.rawdata', {
-				views:{
-					"modal": {
-						templateUrl: 'partials/modalRawData.html'
-					}
-				}
-			});
-			$stateProvider.state('modal.vax', {
-				views:{
-					"modal": {
-						templateUrl: 'partials/modalVax.html'
-					}
-				}
-			});
-			$stateProvider.state('modal.xcom2', {
-				views:{
-					"modal": {
-						templateUrl: 'partials/modalXCom2.html'
-					}
-				}
-			});
-			$stateProvider.state('modal.adultswim', {
-				views:{
-					"modal": {
-						templateUrl: 'partials/modalAdultSwim.html'
-					}
-				}
-			});
-			$stateProvider.state('modal.theblu', {
-				views:{
-					"modal": {
-						templateUrl: 'partials/modalTheBlu.html'
-					}
-				}
-			});
-			$stateProvider.state('modal.bloom', {
-				views:{
-					"modal": {
-						templateUrl: 'partials/modalBloom.html'
-					}
-				}
-			});
-			$stateProvider.state('modal.streetsofindia', {
-				views:{
-					"modal": {
-						templateUrl: 'partials/modalStreetsOfIndia.html'
-					}
-				}
-			});
-			$stateProvider.state('modal.rasterizer', {
-				views:{
-					"modal": {
-						templateUrl: 'partials/modalRasterizer.html'
-					}
-				}
-			});
-			$stateProvider.state('modal.marchingcubes', {
-				views:{
-					"modal": {
-						templateUrl: 'partials/modalMarchingCubes.html'
-					}
-				}
-			});
-			$stateProvider.state('modal.skeletalanim', {
-				views:{
-					"modal": {
-						templateUrl: 'partials/modalSkeletalAnim.html'
-					}
-				}
-			});
-		}
-	);
-
 	app.controller("ProjectController", ["$http", "$scope", function($http, $scope) {
 		$scope.projectList = [];
 		$scope.tagToProjectMap = [];
+		$scope.currentProject = null;
 
 		$http.get("./projects.json").then(
 			function success(response) {
@@ -168,7 +53,76 @@
 			// 	}
 			// }
 		}
+
+		$scope.setProjectByName = function(projectName)
+		{
+			var proj = getProjectByName(projectName);
+			$scope.currentProject = proj;
+		}
+
+		var getProjectByName = function(projectName)
+		{
+			for (var i=0; i<$scope.projectList.length; i++)
+			{
+				if ($scope.projectList[i].title == projectName)
+				{
+					return $scope.projectList[i];
+				}
+			}
+		}
+
+		
+
+		$scope.onClicked = function()
+		{
+			console.log("projectController clicked!");
+		}
 	}]);
+
+	app.directive("projectModal", function($rootScope, $compile)
+	{
+		// 			// Clicking outside of the modal closes it
+		// 			$(document).on('click', '.Modal-backdrop, .Modal-holder', function() {
+		// 				$state.go('base');
+		// 			});
+		// 			$(document).on('click', '.Modal-box, .Modal-box *', function(e) {
+		// 				e.stopPropagation();
+		// 			});
+
+		return {
+			scope: {
+				currentProject: '=selectedProject'
+			},
+			link: function(scope, element, attrs)
+			{
+				scope.projectName = "UNKNOWN PROJECT NAME";
+				scope.leftSideData = [];
+				scope.rightSideMedia = [];
+				scope.isHidden = true;
+
+				scope.setProjectData = function(projectData) 
+				{
+					scope.leftSideData = [];
+					for(var para in projectData.leftContent)
+					{
+						//addParagraph(para);
+						scope.leftSideData.push(para);
+					}
+				};
+
+				var addParagraph = function(paragraphStr)
+				{
+					console.log("Adding paragraph: " + paragraphStr);
+
+					var pElem = "<p>" + paragraphStr + "</p>";
+					var newScope = scope.$new();
+					var newElement = $compile(pElem)(newScope);
+					element.append(newElement);
+				}
+			},
+			templateUrl: "partials/modalTemplate.html"
+		};
+	})
 
 	app.directive("projectItem", function() {
 		return {
@@ -178,10 +132,13 @@
 			scope: {
 				bgimage: '@',
 				projname: '@',
-				state: '@'
 			},
 			link: function(scope, element, attrs, ctrl, transclude) {
-				
+				scope.onClicked = function()
+				{
+					console.log("projectItem clicked!");
+					scope.$parent.setProjectByName(scope.projname);
+				}
 			}
 		};
 	});

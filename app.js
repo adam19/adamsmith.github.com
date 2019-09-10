c4f15b13beec38246f803a109eb35b91abf25bb3
DESKTOP\Adam <adam19@gmail.com>
2019-09-10 00:11:18 -0700

    Fixed more of the modal window problems. Need to work on generating the HTML for media content

--- app.js
+++ app.js
@@ -1,122 +1,6 @@
 (function() {
 	var app = angular.module("Portfolio", ['ui.router', 'ngAnimate', 'ui.bootstrap']);
 
-	app.config(function ($stateProvider, $urlRouterProvider) {
-			$urlRouterProvider.otherwise('modal');
-	
-			$stateProvider.state('base', {
-				url: ''
-			});
-
-			$stateProvider.state('modal', {
-				url: "",
-				views:{
-					"modal": {
-						templateUrl: "partials/modal.html"
-					}
-				},
-				onEnter: function($state){
-					// Hitting the ESC key closes the modal
-					$(document).on('keyup', function(e){
-						if(e.keyCode == 27){
-							$(document).off('keyup')
-							$state.go('base')
-						}
-					});
-				
-					// Clicking outside of the modal closes it
-					$(document).on('click', '.Modal-backdrop, .Modal-holder', function() {
-						$state.go('base');
-					});
-				
-					// Clicking on the modal or it's contents doesn't cause it to close
-					$(document).on('click', '.Modal-box, .Modal-box *', function(e) {
-						e.stopPropagation();
-					});
-				},
-				abstract: true
-			});
-
-			$stateProvider.state('modal.disney', {
-				views:{
-					"modal": {
-						templateUrl: 'partials/modalDisney.html'
-					}
-				}
-			});
-			$stateProvider.state('modal.rawdata', {
-				views:{
-					"modal": {
-						templateUrl: 'partials/modalRawData.html'
-					}
-				}
-			});
-			$stateProvider.state('modal.vax', {
-				views:{
-					"modal": {
-						templateUrl: 'partials/modalVax.html'
-					}
-				}
-			});
-			$stateProvider.state('modal.xcom2', {
-				views:{
-					"modal": {
-						templateUrl: 'partials/modalXCom2.html'
-					}
-				}
-			});
-			$stateProvider.state('modal.adultswim', {
-				views:{
-					"modal": {
-						templateUrl: 'partials/modalAdultSwim.html'
-					}
-				}
-			});
-			$stateProvider.state('modal.theblu', {
-				views:{
-					"modal": {
-						templateUrl: 'partials/modalTheBlu.html'
-					}
-				}
-			});
-			$stateProvider.state('modal.bloom', {
-				views:{
-					"modal": {
-						templateUrl: 'partials/modalBloom.html'
-					}
-				}
-			});
-			$stateProvider.state('modal.streetsofindia', {
-				views:{
-					"modal": {
-						templateUrl: 'partials/modalStreetsOfIndia.html'
-					}
-				}
-			});
-			$stateProvider.state('modal.rasterizer', {
-				views:{
-					"modal": {
-						templateUrl: 'partials/modalRasterizer.html'
-					}
-				}
-			});
-			$stateProvider.state('modal.marchingcubes', {
-				views:{
-					"modal": {
-						templateUrl: 'partials/modalMarchingCubes.html'
-					}
-				}
-			});
-			$stateProvider.state('modal.skeletalanim', {
-				views:{
-					"modal": {
-						templateUrl: 'partials/modalSkeletalAnim.html'
-					}
-				}
-			});
-		}
-	);
-
 	app.controller("ProjectController", ["$http", "$scope", function($http, $scope) {
 		$scope.projectList = [];
 		$scope.tagToProjectMap = [];
@@ -213,14 +97,33 @@
 				scope.rightSideMedia = [];
 				scope.isHidden = true;
 
+				scope.$watch('selectedProject', function(newValue, oldValue)
+				{
+					scope.setProjectData(newValue);
+				}, true);
+
 				scope.setProjectData = function(projectData) 
 				{
+					if (!projectData)
+						return;
+
 					scope.leftSideData = [];
 					for(var para in projectData.leftContent)
 					{
-						//addParagraph(para);
-						scope.leftSideData.push(para);
+						scope.leftSideData.push(projectData.leftContent[para]);
 					}
+
+					scope.rightSideMedia = [];
+					for(var media in projectData.rightContent)
+					{
+						scope.rightSideMedia.push(projectData.rightContent[media]);
+
+						/*
+							* GENERATE THE MEDIA CONTENT AND ATTACH IT TO THE ELEMENT
+						 */
+					}
+					
+					scope.projectName = projectData.title;
 				};
 
 				var addParagraph = function(paragraphStr)
@@ -253,15 +156,7 @@
 					console.log("projectItem " + scope.$id + " clicked!");
 
 					var projname = scope.projname;
-					//scope.$parent.setProjectByName(scope.projname);
 					scope.setProjectByName({projname: projname});
-
-
-
-					// CONTINUE WORKING ON THIS SECTION TO GET THE SCOPE BINDINGS WORKING !!!
-
-
-
 				}
 			}
 		};

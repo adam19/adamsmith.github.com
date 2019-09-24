0f9b00b0d4901cf51221ffd371f8531d35c61ecc
Adam Smith <adam19@gmail.com>
2019-09-24 14:55:29 -0700

    Added basic and HDR texture loading Started a FPS control scheme for debugging

--- threejsApp.js
+++ threejsApp.js
@@ -6,11 +6,28 @@
 //     Scene 
 // } from './node_modules/three/build/three';
 
+import { RGBELoader } from './node_modules/three/examples/jsm/loaders/RGBELoader.js';
+
 var scene;
 var camera;
 var renderer;
+var textureLoader;
+var rgbeTextureLoader;
+
+var controls = {};
+var player = {
+    turnSpeed: .001,
+    speed: .1,
+    velocity: 0
+};
+var keyForward = '87';
+var keyBackward= '83';
+var keyStrafeLeft = '65';
+var keyStrafeRight = '68';
 
 var cube;
+var cubeTexture;
+var cubeMaterial;
 
 var init = function()
 {
@@ -27,12 +44,55 @@ var init = function()
     
     camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
     camera.position.set(0, 0, 10);
+    // camera.position.set(-4, 4, 10);
+
+    controls = new THREE.FirstPersonControls(camera, container);
+    // controls = new THREE.FlyControls(camera, container);
+    
+    document.addEventListener('keydown', ({ keyCode }) => {
+        controls[keyCode] = true;
+    });
+    document.addEventListener('keyup', ({ keyCode }) => {
+        controls[keyCode] = false;
+    });
+    // document.addEventListener('mousemove', ({ event }) => {
+    //     if (!event)
+    //     {
+    //         event = window.event;
+    //     }
+
+    //     var x = event.clientX;
+    //     var y = event.clientY;
+
+    //     controls.isMouseMoving = true;
+
+    //     controls.prevMouseX = controls.currMouseX;
+    //     controls.prevMouseY = controls.currMouseY;
+        
+    //     controls.currMouseX = x;
+    //     controls.currMouseY = y;
+    // });
+
     
     const geom = new THREE.BoxBufferGeometry(2, 2, 2);
     //const mat = new THREE.MeshBasicMaterial();
     const mat = new THREE.MeshStandardMaterial({color: 0xffff00});
-    cube = new THREE.Mesh(geom, mat);
+
+    textureLoader = new THREE.TextureLoader();
+    rgbeTextureLoader = new RGBELoader();
+    rgbeTextureLoader.setType(THREE.FloatType);
+
+    // cubeTexture = textureLoader.load('./scene/textures/rocks_albedo_a.png');
+    cubeTexture = rgbeTextureLoader.load('./scene/textures/sunset.hdr');
+    cubeTexture.encoding = THREE.sRGBEncoding;
+    cubeTexture.anisotropy = 16;
+    cubeMaterial = new THREE.MeshStandardMaterial({
+        map: cubeTexture
+    });
+    
+    cube = new THREE.Mesh(geom, cubeMaterial);
     scene.add(cube);
+
 
     const light = new THREE.DirectionalLight(0xffffff, 5.0)
     light.position.set(10, 10, 10);
@@ -41,6 +101,12 @@ var init = function()
     renderer = new THREE.WebGLRenderer({antialias: true});
     renderer.setSize(container.clientWidth, container.clientHeight);
     renderer.setPixelRatio(window.devicePixelRatio);
+    renderer.gammaFactor = 2.2;
+    renderer.gammaOutput = true;
+
+    renderer.toneMapping = THREE.ReinhardToneMapping;
+    renderer.toneMappingExposure = 2.0;
+
     renderer.setAnimationLoop(() => {
         update();
         draw();
@@ -53,13 +119,46 @@ var init = function()
 
 var update = function()
 {
-    cube.rotation.z += 0.01;
-    cube.rotation.x += 0.01;
-    cube.rotation.y += 0.01;
+    if (controls[keyBackward])
+    {
+        console.log("forward");
+        camera.position.x -= Math.sin(camera.rotation.y) * player.speed;
+        camera.position.z -= -Math.cos(camera.rotation.y) * player.speed;
+    }
+    if (controls[keyForward])
+    {
+        console.log("backward");
+        camera.position.x -= Math.sin(camera.rotation.y) * player.speed;
+        camera.position.z += -Math.cos(camera.rotation.y) * player.speed;
+    }
+    if (controls[keyStrafeRight])
+    {
+        console.log("strafe left");
+        camera.position.x += Math.sin(camera.rotation.y + Math.PI / 2) * player.speed;
+        camera.position.z += -Math.cos(camera.rotation.y + Math.PI / 2) * player.speed;
+    }
+    if (controls[keyStrafeLeft])
+    {
+        console.log("strafe right");
+        camera.position.x += Math.sin(camera.rotation.y - Math.PI / 2) * player.speed;
+        camera.position.z += -Math.cos(camera.rotation.y - Math.PI / 2) * player.speed;
+    }
+
+    // if (controls.isMouseMoving)
+    // {
+    //     var dx = controls.currMouseX - controls.prevMouseX;
+    //     var dy = controls.currMouseY - controls.prevMouseY;
+
+    //     console.log("D(" + dx + "," + dy + ")");
+
+    //     camera.rotation.y += (dy * player.turnSpeed);
+    // }
+    // controls.isMouseMoving = false;
 }
 
 var draw = function()
 {
+    renderer.toneMappingExposure = 2.0;
     renderer.render(scene, camera);
 }
 
